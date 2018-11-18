ca342abb0c5b9686264d703a31fd40c6cb63b5b0
Jerry Lin <hqlyz112@163.com>
2018-11-18 13:16:49 +0800

    update

---
+++ assembly/uremdiv.s
@@ -0,0 +1,19 @@
+	.file	"uremdiv.c"
+	.text
+	.globl	remdiv
+	.type	remdiv, @function
+remdiv:
+.LFB0:
+	.cfi_startproc
+	movq	%rdi, %rax
+	movq	%rdx, %rdi
+	movl	$0, %edx
+	divq	%rsi
+	movq	%rax, (%rdi)
+	movq	%rdx, (%rcx)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	remdiv, .-remdiv
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
